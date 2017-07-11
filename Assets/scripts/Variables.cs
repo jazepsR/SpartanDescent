79f1943b2b1d24e4835db0b4cce4aeede53c5314
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2017-07-11 15:36:20 +0300

    Blend for transition water tiles

--- Assets/scripts/Variables.cs
+++ Assets/scripts/Variables.cs
@@ -13,7 +13,6 @@ public static class Variables {
     public static Transform player;
     public static int health = 5;
     public static int spearCount = 20;
-
     public static int FireTreshold = 10;
     public static int LonelyTreshold = 18;
     public static float waterLevel = 0.53f;
